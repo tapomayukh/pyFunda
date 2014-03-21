096a5fc6c7455d58856294c01eed07de05f5f9b9
Tapomayukh <tapomayukh@gatech.edu>
2014-03-21 17:00:08 -0400

    On branch master 	new file:   level_2.py

---
+++ sandbox_tapo/level_2.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+# Level 1
+
+import pylab as pyl
+import numpy as np
+import matplotlib.pyplot as pp
+import scipy as scp
+import scipy.ndimage as ni
+
+import copy
+import pickle
+import optparse
+import string
+from collections import Counter
+
+import unittest
+
+class Problem:
+    def __init__(self, level_num, input_str):
+        self.level_num = level_num
+        self.input_str = input_str
+         
+    def apply_rule(self):
+        # Params
+        d = {}
+
+        # Body
+        for letter in self.input_str:
+            d[letter] = self.input_str.count(letter)
+        
+        return d
+            
+    def gen_output(self):
+        output = self.apply_rule()
+        
+        #for k in sorted(output):
+            #print (k + ': ' + str(solution[k]))
+            
+        output_str = "".join(ch for ch in self.input_str if output[ch] == 1)  
+        return output_str
+        
+    
+######################################################################################################
+
+if __name__ == '__main__':
+
+    
+    #inp_string = raw_input("Enter a string to be converted:")
+    inp_string = open("ocr.txt").read()
+    
+    level_no = 2
+        
+    level_1 = Problem(level_no, inp_string)
+    solution = level_1.gen_output()        
+        
+    print "The solution to Level ", level_no, " is:", solution
+        
