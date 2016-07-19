03e0eb263ddb45239c99c84a425749630c746378
Tapo <tapo@colossus9.(none)>
2016-07-19 19:04:17 -0400

    Committer: Tapo <tapo@colossus9.(none)> 	modified:   lines_of_code.py

--- lines_of_code.py
+++ lines_of_code.py
@@ -50,36 +50,42 @@ def run_algorithm(path_to_code):
 	save_data_to_google_spreadsheet(lines_data)
 
 def save_data_to_google_spreadsheet(lines_data):
-	scope = ['https://spreadsheets.google.com/feeds']
-	credentials = ServiceAccountCredentials.from_json_keyfile_name('/home/tapo/git/project lines code-85f392792e73.json', scope)
-	gc = gspread.authorize(credentials)
-	wks = gc.open("lines_of_code").sheet1
+    scope = ['https://spreadsheets.google.com/feeds']
+    credentials = ServiceAccountCredentials.from_json_keyfile_name('/home/tapo/git/project lines code-85f392792e73.json', scope)
+    gc = gspread.authorize(credentials)
+    wks = gc.open("lines_of_code").sheet1
 	
-	data = wks.get_all_values()
-	data.append([str(lines_data[0]), str(lines_data[1]), str(lines_data[2]), str(lines_data[3])])
-	data_arr = np.array(data)
-	rows, cols = np.shape(data_arr)
-	for row in range(rows):
-		for col in range(cols):
-			wks.update_cell(row+1,col+1,data_arr[row][col])
+    data = wks.get_all_values()
+    data_arr_prev = np.array(data)
+    rows_prev, cols_prev = np.shape(data_arr_prev)
+    if rows_prev <= 1:
+        prev_lines = 0
+    else:
+        prev_lines = data_arr_prev[rows_prev-1][cols_prev-2] 
+    data.append([str(lines_data[0]),str(lines_data[1]),str(lines_data[2]),str(lines_data[3]),str(float(lines_data[3])- float(prev_lines))])
+    data_arr = np.array(data)
+    rows, cols = np.shape(data_arr)
+    for row in range(rows):
+        for col in range(cols):
+            wks.update_cell(row+1,col+1,data_arr[row][col])
 	
 
 if __name__ == '__main__':
 
-	path_to_git_code1 = '/home/tapo/git/hrl-haptic-manip-dev/sandbox_tapo_darpa_m3/'
-	path_to_git_code2 = '/home/tapo/git/projects_in_c_cpp/'
-	path_to_git_code3 = '/home/tapo/git/projects_in_matlab/'
-	path_to_git_code4 = '/home/tapo/git/projects_in_python/'
-	path_to_git_code5 = '/home/tapo/git/pyFunda/'
-	path_to_svn_robot1_code = '/home/tapo/svn/robot1/usr/tapo/'
-	path_to_svn_robot1_data_code = '/home/tapo/svn/robot1_data/usr/tapo/data_code/'
-	path_to_code = [path_to_git_code1, path_to_git_code2, path_to_git_code3, path_to_git_code4, path_to_git_code5, path_to_svn_robot1_code, path_to_svn_robot1_data_code]
+    path_to_git_code1 = '/home/tapo/git/hrl-haptic-manip-dev/sandbox_tapo_darpa_m3/'
+    path_to_git_code2 = '/home/tapo/git/projects_in_c_cpp/'
+    path_to_git_code3 = '/home/tapo/git/projects_in_matlab/'
+    path_to_git_code4 = '/home/tapo/git/projects_in_python/'
+    path_to_git_code5 = '/home/tapo/git/pyFunda/'
+    path_to_svn_robot1_code = '/home/tapo/svn/robot1/usr/tapo/'
+    path_to_svn_robot1_data_code = '/home/tapo/svn/robot1_data/usr/tapo/data_code/'
+    path_to_code = [path_to_git_code1, path_to_git_code2, path_to_git_code3, path_to_git_code4, path_to_git_code5, path_to_svn_robot1_code, path_to_svn_robot1_data_code]
 
-	schedule.every().day.at("23:59").do(run_algorithm, path_to_code)
+    schedule.every().day.at("23:59").do(run_algorithm, path_to_code)
 
-	while True:
-		schedule.run_pending()
-		time.sleep(1)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 	
 	
 	
