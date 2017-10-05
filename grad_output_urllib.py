04a1f9040c2633def6491627aacd1a19a2264ae9
tapomayukh <tapomayukh@gmail.com>
2017-10-05 12:21:04 -0700

    modified:   grad_output_urllib.py 	modified:   lines_of_code.py

--- grad_output_urllib.py
+++ grad_output_urllib.py
@@ -17,82 +17,87 @@ apiKey = "Write your key here"
 fileDirectory = "your path"
 filePrefix = "ProductivityData"
 
-def save_data_to_google_spreadsheet(filePath):
-	scope = ['https://spreadsheets.google.com/feeds']
-	credentials = ServiceAccountCredentials.from_json_keyfile_name('your json file', scope)
-	gc = gspread.authorize(credentials)
-	wks = gc.open("grad_output").sheet1
+## Self explanatory function : Opens rescuetime data stored in a csv file, stores
+## in a google spreadsheet (which creates the graph), and website embeds it
 
-	with open(filePath, 'rb') as csvfile:
-		datareader = csv.reader(csvfile)
-		i=1
-		j=1
-		temp = 0.0
-		for row in datareader:
-			if row[0][0:4] != 'Date':
-				year = int(float(row[0][0:4]))
-				month = int(float(row[0][5:7]))
-				day = int(float(row[0][8:10]))
-				week_idx = datetime.datetime(year,month,day).weekday()
-				if row[-1] == '1':
-					temp = temp + float(row[1])
-					j=j+1
-				if row[-1] == '2':
-					temp = temp + float(row[1])
-					j=j+1
-				if j == 3:
-					if week_idx == 0:
-						week_day = 'Monday'
-					elif week_idx == 1:
-						week_day = 'Tuesday'
-					elif week_idx == 2:
-						week_day = 'Wednesday'
-					elif week_idx == 3:
-						week_day = 'Thursday'
-					elif week_idx == 4:
-						week_day = 'Friday'
-					elif week_idx == 5:
-						week_day = 'Saturday'
-					else:
-						week_day = 'Sunday'
-					new_row = [week_day, np.str(temp/(14*3600.))]
-					wks.insert_row(new_row,i)
-					i=i+1
-					j=1
-					temp = 0.0
+def save_data_to_google_spreadsheet(filePath):
+    scope = ['https://spreadsheets.google.com/feeds']
+    credentials = ServiceAccountCredentials.from_json_keyfile_name('your json file', scope)
+    gc = gspread.authorize(credentials)
+    wks = gc.open("grad_output").sheet1
+
+    with open(filePath, 'rb') as csvfile:
+	datareader = csv.reader(csvfile)
+	i=1
+	j=1
+	temp = 0.0
+	for row in datareader:
+            if row[0][0:4] != 'Date':
+		year = int(float(row[0][0:4]))
+		month = int(float(row[0][5:7]))
+		day = int(float(row[0][8:10]))
+		week_idx = datetime.datetime(year,month,day).weekday()
+		if row[-1] == '1':
+	            temp = temp + float(row[1])
+		    j=j+1
+		if row[-1] == '2':
+		    temp = temp + float(row[1])
+		    j=j+1
+		if j == 3:
+		    if week_idx == 0:
+			week_day = 'Monday'
+		    elif week_idx == 1:
+			week_day = 'Tuesday'
+		    elif week_idx == 2:
+			week_day = 'Wednesday'
+		    elif week_idx == 3:
+		        week_day = 'Thursday'
+		    elif week_idx == 4:
+			week_day = 'Friday'
+		    elif week_idx == 5:
+			week_day = 'Saturday'
+		    else:
+		        week_day = 'Sunday'
+		    new_row = [week_day, np.str(temp/(14*3600.))]
+		    wks.insert_row(new_row,i)
+		    i=i+1
+		    j=1
+		    temp = 0.0
 				
 
+## Again pretty self-explanatory: Collects data in a 7-day format similar to the Ph.D comic strip which I am trying to emulate here.. (refer www.tapomayukh.com)
 
 def run_algorithm():
-	today = datetime.datetime.now()
-	start_date = today + datetime.timedelta(days=-6)
-	start_day = np.str(start_date.day)
-	start_month = np.str(start_date.month)
-	start_year = np.str(start_date.year)
+    today = datetime.datetime.now()
+    start_date = today + datetime.timedelta(days=-6)
+    start_day = np.str(start_date.day)
+    start_month = np.str(start_date.month)
+    start_year = np.str(start_date.year)
 
-	end_date = datetime.datetime.now()
-	end_day = np.str(end_date.day)
-	end_month = np.str(end_date.month)
-	end_year = np.str(end_date.year)
+    end_date = datetime.datetime.now()
+    end_day = np.str(end_date.day)
+    end_month = np.str(end_date.month)
+    end_year = np.str(end_date.year)
 
 
-	date_s = start_year+'-'+start_month+'-'+start_day
-	date_e = end_year+'-'+end_month+'-'+end_day
-	print "Getting Data for Interval", date_s, "to", date_e
-	params = urllib.urlencode({'key':apiKey, 'perspective':'interval', 'format':'csv', 'restrict_begin':date_s, 'restrict_end':date_e, 'restrict_kind':'productivity', 'resolution_time':'day'})
-	u = urllib.urlopen("https://www.rescuetime.com/anapi/data", params)
-	CSVdata = u.read()
-	filePath = fileDirectory + filePrefix + ".csv"
-	f = open(filePath, "w")
-	f.write(CSVdata)
-	f.close()
-	save_data_to_google_spreadsheet(filePath)
+    date_s = start_year+'-'+start_month+'-'+start_day
+    date_e = end_year+'-'+end_month+'-'+end_day
+    print "Getting Data for Interval", date_s, "to", date_e
+    params = urllib.urlencode({'key':apiKey, 'perspective':'interval', 'format':'csv', 'restrict_begin':date_s, 'restrict_end':date_e, 'restrict_kind':'productivity', 'resolution_time':'day'})
+    u = urllib.urlopen("https://www.rescuetime.com/anapi/data", params)
+    CSVdata = u.read()
+    filePath = fileDirectory + filePrefix + ".csv"
+    f = open(filePath, "w")
+    f.write(CSVdata)
+    f.close()
+    save_data_to_google_spreadsheet(filePath)
 
 if __name__ == '__main__':
 
-	schedule.every().day.at("23:00").do(run_algorithm)
+    # Runs at the end of day
+    schedule.every().day.at("23:59").do(run_algorithm)
 
-	while True:
-		schedule.run_pending()
-		time.sleep(1)
+    while True:
+    	schedule.run_pending()
+	time.sleep(1)
 
--- lines_of_code.py
+++ lines_of_code.py
@@ -18,38 +18,38 @@ import os, os.path
 import csv
 
 class Cnt_Lines:
-	def __init__(self, paths):
-		self.paths = paths
-		today = datetime.datetime.now()
-		self.day = today.day
-		self.month = today.month
-		self.year = today.year
-		self.lines = 0
+    def __init__(self, paths):
+	self.paths = paths
+	today = datetime.datetime.now()
+	self.day = today.day
+	self.month = today.month
+	self.year = today.year
+	self.lines = 0
 
-	def count_lines(self):
-		for path_code in self.paths:
-			for path,dirs,fs in os.walk(path_code):
-				for f in fs:
-					if f.lower().endswith('.py') or f.lower().endswith('.sh') or f.lower().endswith('.launch') or f.lower().endswith('.c') or f.lower().endswith('.cpp') or f.lower().endswith('.h') or f.lower().endswith('.m'):
-						full_path  = path + '/' + f
-						if os.path.isfile(full_path):
-							with open(full_path) as f:
-								self.lines = self.lines + sum(1 for _ in f if _.rstrip())
+    def count_lines(self):
+	for path_code in self.paths:
+            for path,dirs,fs in os.walk(path_code):
+		for f in fs:
+	            if f.lower().endswith('.py') or f.lower().endswith('.sh') or f.lower().endswith('.launch') or f.lower().endswith('.c') or f.lower().endswith('.cpp') or f.lower().endswith('.h') or f.lower().endswith('.m'):
+		        full_path  = path + '/' + f
+			if os.path.isfile(full_path):
+			    with open(full_path) as f:
+				self.lines = self.lines + sum(1 for _ in f if _.rstrip())
 
-	def return_lines(self):
-		return self.lines
+    def return_lines(self):
+	return self.lines
 	
-	def return_date(self):
-		return self.month, self.day, self.year
+    def return_date(self):
+	return self.month, self.day, self.year
 
 def run_algorithm(path_to_code):
-	code = Cnt_Lines(path_to_code)
-	code.count_lines()
-	lines_today = code.return_lines()
-	today_month, today_day, today_year = code.return_date()
-	lines_data = [today_month, today_day, today_year, lines_today]
+    code = Cnt_Lines(path_to_code)
+    code.count_lines()
+    lines_today = code.return_lines()
+    today_month, today_day, today_year = code.return_date()
+    lines_data = [today_month, today_day, today_year, lines_today]
 		
-	save_data_to_google_spreadsheet(lines_data)
+    save_data_to_google_spreadsheet(lines_data)
 
 def save_data_to_google_spreadsheet(lines_data):
     scope = ['https://spreadsheets.google.com/feeds']
@@ -77,7 +77,11 @@ if __name__ == '__main__':
     path_to_git_code5 = '/home/tapo/git/pyFunda/'
     path_to_svn_robot1_code = '/home/tapo/svn/robot1/usr/tapo/'
     path_to_svn_robot1_data_code = '/home/tapo/svn/robot1_data/usr/tapo/data_code/'
-    path_to_code = [path_to_git_code1, path_to_git_code2, path_to_git_code3, path_to_git_code4, path_to_git_code5, path_to_svn_robot1_code, path_to_svn_robot1_data_code]
+    path_to_pr_code = '/home/tapo/my-workspace/src/'
+    path_to_account_setup_code = '/home/tapo/git/shells_aliases/'
+    path_to_dropbox_code='/home/tapo/Dropbox/Personal/patrol/'
+    #path_to_code = [path_to_git_code1, path_to_git_code2, path_to_git_code3, path_to_git_code4, path_to_git_code5, path_to_svn_robot1_code, path_to_svn_robot1_data_code, path_to_dropbox_code]
+    path_to_code = [path_to_git_code2, path_to_git_code3, path_to_git_code4, path_to_git_code5, path_to_pr_code, path_to_account_setup_code, path_to_dropbox_code]
 
     schedule.every().day.at("23:59").do(run_algorithm, path_to_code)
 
